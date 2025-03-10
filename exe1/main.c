08af9ddbd78ff175227e4435a0d80c8506fab985
Vinicius Grecco <viniciusgfm@al.insper.edu.br>
2025-03-10 10:53:40 -0300

    teeste

--- exe1/main.c
+++ exe1/main.c
@@ -5,12 +5,15 @@
 const int BTN_PIN_R = 28;
 const int LED_PIN_R = 4;
 
-volatile int flag_f_r = 0;
+volatile int timer_running = 0;
+volatile int last_time = 0;
 
-void btn_callback(uint gpio, uint32_t events) {
-    if (events == 0x4) { // fall edge
-        flag_f_r = 1;
-    } else if (events == 0x8) { // rise edge
+void btn_callback(int gpio, int events) { 
+    if (events == GPIO_IRQ_EDGE_FALL) { 
+        timer_running = !timer_running;
+        if (!timer_running) {
+            gpio_put(LED_PIN_R, 0);
+        }
     }
 }
 
@@ -18,18 +21,20 @@ int main() {
     stdio_init_all();
     gpio_init(LED_PIN_R);
     gpio_set_dir(LED_PIN_R, GPIO_OUT);
+    gpio_put(LED_PIN_R, 0); 
 
     gpio_init(BTN_PIN_R);
     gpio_set_dir(BTN_PIN_R, GPIO_IN);
     gpio_pull_up(BTN_PIN_R);
+    gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true, (gpio_irq_callback_t)btn_callback);
 
-    gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
-                                       &btn_callback);
-
-    while (true) {
-
-        if (flag_f_r) {
-            flag_f_r = 0;
+    while (1) {
+        if (timer_running) {
+            int current_time = to_ms_since_boot(get_absolute_time());
+            if (current_time - last_time >= 500) { 
+                gpio_put(LED_PIN_R, !gpio_get(LED_PIN_R)); 
+                last_time = current_time;
+            }
         }
     }
 }
