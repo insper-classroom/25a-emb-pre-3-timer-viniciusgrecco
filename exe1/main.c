dc9e960af82115c71e71c48c520ea80bf1f04a78
Vinicius Grecco <viniciusgfm@al.insper.edu.br>
2025-03-13 16:59:23 -0300

    fix

--- exe1/main.c
+++ exe1/main.c
@@ -5,15 +5,21 @@
 const int BTN_PIN_R = 28;
 const int LED_PIN_R = 4;
 
-volatile int timer_running = 0;
-volatile int last_time = 0;
+volatile int flag_f_r = 0;
+volatile bool timer_active = false;
+volatile bool led_state = false;
+repeating_timer_t timer;
 
-void btn_callback(int gpio, int events) { 
-    if (events == GPIO_IRQ_EDGE_FALL) { 
-        timer_running = !timer_running;
-        if (!timer_running) {
-            gpio_put(LED_PIN_R, 0);
-        }
+bool timer_callback(repeating_timer_t *rt) {
+    led_state = !led_state;
+    gpio_put(LED_PIN_R, led_state);
+    return true; 
+}
+
+
+void btn_callback(uint gpio, uint32_t events) {
+    if (events == GPIO_IRQ_EDGE_FALL) {
+        flag_f_r = 1;
     }
 }
 
@@ -21,19 +27,25 @@ int main() {
     stdio_init_all();
     gpio_init(LED_PIN_R);
     gpio_set_dir(LED_PIN_R, GPIO_OUT);
-    gpio_put(LED_PIN_R, 0); 
+    gpio_put(LED_PIN_R, 0);
 
     gpio_init(BTN_PIN_R);
     gpio_set_dir(BTN_PIN_R, GPIO_IN);
     gpio_pull_up(BTN_PIN_R);
-    gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true, (gpio_irq_callback_t)btn_callback);
 
-    while (1) {
-        if (timer_running) {
-            int current_time = to_ms_since_boot(get_absolute_time());
-            if (current_time - last_time >= 500) { 
-                gpio_put(LED_PIN_R, !gpio_get(LED_PIN_R)); 
-                last_time = current_time;
+    gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true, &btn_callback);
+
+    while (true) {
+        if (flag_f_r) {
+            flag_f_r = 0;
+            if (timer_active) {
+                cancel_repeating_timer(&timer);
+                gpio_put(LED_PIN_R, 0);
+                led_state = false;
+                timer_active = false;
+            } else {
+                add_repeating_timer_ms(500, timer_callback, NULL, &timer);
+                timer_active = true;
             }
         }
     }
