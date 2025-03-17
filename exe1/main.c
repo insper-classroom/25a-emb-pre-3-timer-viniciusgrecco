6afd5e6638c0139542f9948de42e93d88ff5d019
Vinicius Grecco <viniciusgfm@al.insper.edu.br>
2025-03-16 23:15:21 -0300

    exec1

--- exe1/main.c
+++ exe1/main.c
@@ -5,21 +5,20 @@
 const int BTN_PIN_R = 28;
 const int LED_PIN_R = 4;
 
-volatile int flag_f_r = 0;       
-volatile bool timer_active = false; 
-volatile bool led_state = false; 
-repeating_timer_t timer;        
-
-bool timer_callback(repeating_timer_t *rt) {
-    led_state = !led_state;
-    gpio_put(LED_PIN_R, led_state);
-    return true; 
-}
+volatile bool button_pressed = false;
+volatile bool timer_fired = false;
+volatile bool timer_running = false;
+repeating_timer_t timer_0;
 
 void btn_callback(uint gpio, uint32_t events) {
-    if (events & GPIO_IRQ_EDGE_FALL) {
-        flag_f_r = 1;
+    if (events == 0x4) { // Falling edge
+        button_pressed = true;
     }
+}
+
+bool timer_0_callback(repeating_timer_t *rt) {
+    timer_fired = true;
+    return true; 
 }
 
 int main() {
@@ -29,27 +28,36 @@ int main() {
     gpio_set_dir(LED_PIN_R, GPIO_OUT);
     gpio_put(LED_PIN_R, 0);
 
+   
     gpio_init(BTN_PIN_R);
     gpio_set_dir(BTN_PIN_R, GPIO_IN);
     gpio_pull_up(BTN_PIN_R);
     gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true, &btn_callback);
 
-    while (true) {
-        if (flag_f_r) {
-            flag_f_r = 0; 
+   
+    int timer_hz = 2;
 
-            if (!timer_active) {
-                if (add_repeating_timer_ms(500, timer_callback, NULL, &timer)) {
-                    timer_active = true;
+    while (true) {
+        if (button_pressed) {
+            button_pressed = false;  
+            if (!timer_running) {
+                if (add_repeating_timer_us(1000000 / timer_hz, timer_0_callback, NULL, &timer_0)) {
+                    timer_running = true;
                 } else {
-                    printf("Falha ao adicionar timer\n");
+                    printf("Failed to add timer\n");
                 }
             } else {
-                cancel_repeating_timer(&timer);
-                timer_active = false;
-                led_state = false;
+                cancel_repeating_timer(&timer_0);
+                timer_running = false;
                 gpio_put(LED_PIN_R, 0);
             }
+            sleep_ms(50);
+        }
+
+        if (timer_running && timer_fired) {
+            bool led_state = gpio_get(LED_PIN_R);
+            gpio_put(LED_PIN_R, !led_state);
+            timer_fired = false; 
         }
     }
     return 0;
